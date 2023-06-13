d672c0e59e4191f326c1f853276a8dc16e3e7656
Magnus Dalen Kvalevåg <magnus.kvalevag@ntnu.no>
2023-06-13 12:26:34 +0000

    Fix x, y, and z properties of LinearScan

--- pyuff/objects/scans/linear_scan.py
+++ pyuff/objects/scans/linear_scan.py
@@ -42,3 +42,22 @@ class LinearScan(Scan):
     def reference_distance(self):
         "Distance used for the calculation of the phase term"
         return self.z
+
+    # Unlike the base scan object (pyuff.Scan), x, y, and z are not compulsory
+    # properties, but calculated based on azimuth_axis, depth_axis, and origin.
+    @dependent_property
+    def x(self):
+        X, Z = np.meshgrid(self.x_axis, self.z_axis, indexing="ij")
+        N_pixels = X.size
+        return np.reshape(X, [N_pixels])
+
+    @dependent_property
+    def y(self):
+        N_pixels = self.N_x_axis * self.N_z_axis
+        return np.zeros((N_pixels,))
+
+    @dependent_property
+    def z(self):
+        X, Z = np.meshgrid(self.x_axis, self.z_axis, indexing="ij")
+        N_pixels = Z.size
+        return np.reshape(Z, [N_pixels])
