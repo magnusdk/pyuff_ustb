6b578d68ae631e36852adab5e732500e07b388b9
Magnus Dalen Kvalevåg <magnus.kvalevag@ntnu.no>
2023-06-14 10:37:03 +0000

    Add __init__ files to scans and probes

---
+++ pyuff/objects/probes/__init__.py
@@ -0,0 +1,13 @@
+from pyuff.objects.probes.curvilinear_array import CurvilinearArray
+from pyuff.objects.probes.curvilinear_matrix_array import CurvilinearMatrixArray
+from pyuff.objects.probes.linear_array import LinearArray
+from pyuff.objects.probes.matrix_array import MatrixArray
+from pyuff.objects.probes.probe import Probe
+
+__all__ = [
+    "CurvilinearArray",
+    "CurvilinearMatrixArray",
+    "LinearArray",
+    "MatrixArray",
+    "Probe",
+]
---
+++ pyuff/objects/scans/__init__.py
@@ -0,0 +1,13 @@
+from pyuff.objects.scans.linear_3D_scan import Linear3DScan
+from pyuff.objects.scans.linear_scan import LinearScan
+from pyuff.objects.scans.linear_scan_rotated import LinearScanRotated
+from pyuff.objects.scans.scan import Scan
+from pyuff.objects.scans.sector_scan import SectorScan
+
+__all__ = [
+    "Linear3DScan",
+    "LinearScan",
+    "LinearScanRotated",
+    "Scan",
+    "SectorScan",
+]
