65d42eaaa7d5d13a75446e16a92ed96708653703
Magnus Dalen Kvalevåg <magnus.kvalevag@ntnu.no>
2023-06-22 16:52:49 +0200

    Include lazy_operations module in lazy_arrays

--- pyuff_ustb/readers/lazy_arrays/__init__.py
+++ pyuff_ustb/readers/lazy_arrays/__init__.py
@@ -1,3 +1,4 @@
+from pyuff_ustb.readers.lazy_arrays import lazy_operations
 from pyuff_ustb.readers.lazy_arrays.base import LazyArray, LazyScalar
 
-__all__ = ["LazyArray", "LazyScalar"]
+__all__ = ["lazy_operations", "LazyArray", "LazyScalar"]
